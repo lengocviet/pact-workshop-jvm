d8790db2d1cd856efe66dc1d28247f80734d739d
viet.le@logigear.com <viet.le@logigear.com>
2020-10-19 21:45:34 +0700

    Separate contract vs integration test

---
+++ consumer/src/test/java/integration/TestAssert.java
@@ -0,0 +1,11 @@
+package integration;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestAssert {
+    @Test
+    public void forAssert() {
+        assertEquals(1, 1);
+    }
+}
